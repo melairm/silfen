3fb1a3b113df15c9fe7d22e50ba8dccdbf64cca2
melairm <mela3411@stud.kea.dk>
2021-11-18 16:57:40 +0100

    final

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-const urlParams = new URLSearchParams(windows.location.search);
-const id = urlParams.get("id");
-fetch("http://mcvcreate.com/wp_silfen/wp-json/wp/v2/bag?_embed" + id)
+fetch("http://mcvcreate.com/wp_silfen/wp-json/wp/v2/bag/?_embed")
   .then((res) => res.json())
   .then(gotData);
 
@@ -11,6 +9,9 @@ function showBags(bags) {
   console.log(bags);
   const shCard = document.querySelector("#bagtemplate").content;
   const clone = shCard.cloneNode(true);
+  clone
+    .querySelector("a")
+    .setAttribute("href", "productpage.html?id=" + bags.id);
   clone.querySelector(".name").textContent = bags.title["rendered"];
   clone.querySelector("img").src =
     bags._embedded["wp:featuredmedia"][0].media_details.sizes.full.source_url;
@@ -18,3 +19,12 @@ function showBags(bags) {
   const parent = document.querySelector(".small-image");
   parent.appendChild(clone);
 }
+const toggleButton = document.getElementsByClassName("toggle-button")[0];
+const navBarLinks = document.getElementsByClassName("navbar-links")[0];
+
+toggleButton.addEventListener("click", () => {
+  navBarLinks.classList.toggle("active");
+});
+$(".navbar-links").click(function () {
+  navbarLinks.classList.toggle("active");
+});
