f52b95cdb95dfc323aeb0af2297ceba692bf45b0
melairm <mela3411@stud.kea.dk>
2021-11-18 13:03:58 +0100

    fetching

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-fetch("http://mcvcreate.com/wp_silfen/wp-json/wp/v2/bag?_embed")
+const urlParams = new URLSearchParams(windows.location.search);
+const id = urlParams.get("id");
+fetch("http://mcvcreate.com/wp_silfen/wp-json/wp/v2/bag?_embed" + id)
   .then((res) => res.json())
   .then(gotData);
 
@@ -9,10 +11,10 @@ function showBags(bags) {
   console.log(bags);
   const shCard = document.querySelector("#bagtemplate").content;
   const clone = shCard.cloneNode(true);
-  clone.querySelector(".name").textContent = bags.name;
+  clone.querySelector(".name").textContent = bags.title["rendered"];
   clone.querySelector("img").src =
     bags._embedded["wp:featuredmedia"][0].media_details.sizes.full.source_url;
   clone.querySelector(".price").textContent = bags.price;
-  const parent = document.querySelector(".container");
+  const parent = document.querySelector(".small-image");
   parent.appendChild(clone);
 }
